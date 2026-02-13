1bab76f2573cb186ef361741953c2bd432ee50dc
Jayank Chaudhary <jayankchaudhary@gmail.com>
2026-02-13 22:05:55 +0530

    Initial MyBankApp commit

---
+++ app/src/main/java/com/example/mybankapp/BankAccount.kt
@@ -0,0 +1,17 @@
+    package com.example.mybankapp
+
+
+    import androidx.compose.runtime.getValue
+    import androidx.compose.runtime.mutableIntStateOf
+    import androidx.compose.runtime.mutableStateListOf
+    import androidx.compose.runtime.mutableStateOf
+    import androidx.compose.runtime.setValue
+
+    class BankAccount(
+        val username: String,
+        initialBalance: Int
+    ) {
+        // Uses mutableIntStateOf for automatic UI updates
+        var balance by mutableIntStateOf(initialBalance)
+        val transactions = mutableStateListOf<Transaction>()
+    }
